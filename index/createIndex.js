93a5b823e0056b792f55d55102e911e9d4cef77c
Marco Mendão <mac.mendao@gmail.com>
2022-02-15 20:16:03 +0000

    Add first commit

---
+++ index/createIndex.js
@@ -0,0 +1,6 @@
+
+// What is the index name?
+// What is the number of shard?
+// What is the number of replicas?
+// books
+// Index created
---
+++ index/deleteIndex.js
@@ -0,0 +1,4 @@
+
+// What is the index name?
+// books
+// Index deleted
